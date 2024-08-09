1740ffc84b9357e8a78dcb40dc70f13ad8852b16
sh4rkd <developer@fredmiramontes.dev>
2024-08-09 13:17:42 -0600

    Fix: rename src map

--- src/components/MapView.tsx
+++ src/components/MapView.tsx
@@ -12,7 +12,7 @@ const MapView: React.FC = () => {
         </div>
       </div>
       <div className="bg-gray-200 h-full rounded-lg">
-        <img src={'../src/assets/map.svg'} alt="" />
+        <img src={new URL('../assets/Map.png', import.meta.url).href} alt="" />
       </div>
     </div>
   );
