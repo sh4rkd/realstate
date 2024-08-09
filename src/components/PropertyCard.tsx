f60d9918d83325083fb6e0bc39164305a780a4fd
sh4rkd <developer@fredmiramontes.dev>
2024-08-09 13:42:16 -0600

    Fix: change locator src and fixing like.svg

--- src/components/PropertyCard.tsx
+++ src/components/PropertyCard.tsx
@@ -20,10 +20,7 @@ const PropertyCard: React.FC<PropertyCardProps> = ({ name, price, location, imag
             <div>
               <h3 className="font-bold text-xl mb-2">$ {price}/ Year</h3>
               <p className="text-gray-600 text-sm mb-2 flex items-center font-inter">
-                <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4 mr-1" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z" />
-                  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M15 11a3 3 0 11-6 0 3 3 0 016 0z" />
-                </svg>
+                <img src={new URL('../assets/icons/location.svg', import.meta.url).href} alt="" />
                 {location}
               </p>
               <hr className="w-10 h-1 my-4 border-orange-500 rounded bg-orange-300"></hr>
@@ -31,7 +28,7 @@ const PropertyCard: React.FC<PropertyCardProps> = ({ name, price, location, imag
             <p className="text-gray-500 text-sm">Overview</p>
           </div>
           <div className="py-3 px-3 cursor-cell">
-              <img src="./src/assets/like.svg" alt="" />
+              <img src={new URL("../assets/like.svg", import.meta.url).href} alt="" />
           </div>
         </div>
       </div>
